907bdfffb3b31652cf2ef14ab8485b1ad2bf441a
Rita Zhang <rita.z.zhang@gmail.com>
2019-05-09 10:23:31 -0700

    support pod identity

--- pkg/secrets-store/nodeserver.go
+++ pkg/secrets-store/nodeserver.go
@@ -43,7 +43,7 @@ const (
 )
 
 func (ns *nodeServer) NodePublishVolume(ctx context.Context, req *csi.NodePublishVolumeRequest) (*csi.NodePublishVolumeResponse, error) {
-	glog.V(0).Infof("NodeUnpublishVolume")
+	glog.V(0).Infof("NodePublishVolume")
 	// Check arguments
 	if req.GetVolumeCapability() == nil {
 		return nil, status.Error(codes.InvalidArgument, "Volume capability missing in request")
@@ -59,6 +59,8 @@ func (ns *nodeServer) NodePublishVolume(ctx context.Context, req *csi.NodePublis
 	}
 
 	targetPath := req.GetTargetPath()
+	publishContext := req.GetPublishContext()
+	glog.V(2).Infof("secrets-store - publishContext %v", publishContext)
 	notMnt, err := mount.New("").IsLikelyNotMountPoint(targetPath)
 	if err != nil && !os.IsNotExist(err) {
 		return nil, err
