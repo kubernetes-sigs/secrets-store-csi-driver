2913d5d89d8b0ea04fd2c20dd472480dc9deaa17
Anish Ramasekar <anish.ramasekar@gmail.com>
2019-11-08 14:16:16 -0800

    check is mounted before unmount

--- pkg/secrets-store/nodeserver.go
+++ pkg/secrets-store/nodeserver.go
@@ -231,6 +231,7 @@ func (ns *nodeServer) NodePublishVolume(ctx context.Context, req *csi.NodePublis
 			return nil, fmt.Errorf("error mounting secret %v", stderr.String())
 		}
 	} else {
+		// mock provider is used only for running sanity tests against the driver
 		err = mounter.Mount("tmpfs", targetPath, "tmpfs", []string{})
 		if err != nil {
 			log.Errorf("mount err: %v", err)
@@ -241,8 +242,12 @@ func (ns *nodeServer) NodePublishVolume(ctx context.Context, req *csi.NodePublis
 
 	notMnt, err = mount.New("").IsLikelyNotMountPoint(targetPath)
 	if err != nil {
-		log.Errorf("Error checking IsLikelyNotMountPoint: %v", err)
+		log.Errorf("Error checking targetPath %s IsLikelyNotMountPoint: %v", targetPath, err)
 		return nil, err
+	}
+	// this means the mount didn't succeed
+	if notMnt {
+		return nil, fmt.Errorf("after MountSecretsStoreObjectContent, notMnt: %v", notMnt)
 	}
 	log.Debugf("after MountSecretsStoreObjectContent, notMnt: %v", notMnt)
 	return &csi.NodePublishVolumeResponse{}, nil
@@ -260,14 +265,30 @@ func (ns *nodeServer) NodeUnpublishVolume(ctx context.Context, req *csi.NodeUnpu
 	targetPath := req.GetTargetPath()
 	volumeID := req.GetVolumeId()
 
+	// check if target path is mount point
+	notMnt, err := mount.New("").IsLikelyNotMountPoint(targetPath)
+	if (err != nil && os.IsNotExist(err)) || notMnt {
+		log.Infof("target path %s is not likely a mount point, notMnt %v", targetPath, notMnt)
+		return &csi.NodeUnpublishVolumeResponse{}, nil
+	}
+	if err != nil && !os.IsNotExist(err) {
+		return nil, err
+	}
+	// if mount point, ensure mount link is still valid
+	if !notMnt {
+		if _, err = ioutil.ReadDir(targetPath); err != nil {
+			log.Infof("secrets-store not mounted to target path %s", targetPath)
+			return &csi.NodeUnpublishVolumeResponse{}, nil
+		}
+	}
+
 	// Unmounting the target
-	err := mount.New("").Unmount(req.GetTargetPath())
+	err = mount.New("").Unmount(targetPath)
 	if err != nil {
 		log.Errorf("error unmounting target path %s, err: %v", targetPath, err)
 		return nil, status.Error(codes.Internal, err.Error())
 	}
 	log.Debugf("secrets-store: targetPath %s volumeID %s has been unmounted.", targetPath, volumeID)
-
 	return &csi.NodeUnpublishVolumeResponse{}, nil
 }
 
