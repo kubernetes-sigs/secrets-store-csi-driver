d272a25c65485147701538a19992c64e4dbd76f3
Nilekh Chaudhari <1626598+nilekhc@users.noreply.github.com>
2021-07-13 11:31:46 -0700

    test: push crd image for aws e2e tests
    Signed-off-by: Nilekh Chaudhari <1626598+nilekhc@users.noreply.github.com>

--- test/scripts/initialize_eks_cluster.bash
+++ test/scripts/initialize_eks_cluster.bash
@@ -10,16 +10,20 @@ fi
 AWS_ACCOUNT_ID=$(aws --region $AWS_REGION sts get-caller-identity --query Account --output text)
 
 ECR_REGISTRY_NAME="driver"
+ECR_CRD_REGISTRY_NAME="driver-crds"
 ECR_REGISTRY_URL="$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com"
 ECR_REGISTRY_URL_WITH_NAME="$ECR_REGISTRY_URL/$ECR_REGISTRY_NAME"
+ECR_CRD_REGISTRY_URL_WITH_NAME="$ECR_REGISTRY_URL/$ECR_CRD_REGISTRY_NAME"
 
 IMAGE_TAG=$ECR_REGISTRY_URL_WITH_NAME:$IMAGE_VERSION
+CRD_IMAGE_TAG=$ECR_CRD_REGISTRY_URL_WITH_NAME:$IMAGE_VERSION
 NAMESPACE="kube-system"
 AWS_SERVICE_ACCOUNT_NAME="basic-test-mount-sa"
 
 aws --region $AWS_REGION ecr get-login-password | docker login --username AWS --password-stdin $ECR_REGISTRY_URL
 REGISTRY=$ECR_REGISTRY_URL make container
 docker push $IMAGE_TAG
+docker push $CRD_IMAGE_TAG
 
 eksctl create cluster --name $EKS_CLUSTER_NAME --node-type m5.large --region $AWS_REGION 
 eksctl utils associate-iam-oidc-provider --name $EKS_CLUSTER_NAME --approve --region $AWS_REGION
